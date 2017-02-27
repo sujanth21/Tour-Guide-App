b74e1031c5587d8f077875c30a0d05c25774e010
Sujanth <sujanth1987@gmail.com>
2017-02-27 14:14:48 +1100

    Added activities

---
+++ app/src/main/java/com/sujanth/tourguideapp/AttractionActivity.java
@@ -0,0 +1,13 @@
+package com.sujanth.tourguideapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AttractionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_attraction);
+    }
+}
--- app/src/main/java/com/sujanth/tourguideapp/MainActivity.java
+++ app/src/main/java/com/sujanth/tourguideapp/MainActivity.java
@@ -1,7 +1,10 @@
 package com.sujanth.tourguideapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button attractionsButton = (Button) findViewById(R.id.attractions_button);
+        attractionsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent attractionActivity = new Intent(MainActivity.this, AttractionActivity.class);
+                startActivity(attractionActivity);
+            }
+        });
+
+
+        Button restaurantButton = (Button) findViewById(R.id.restaurant_button);
+        restaurantButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent restaurantActivity = new Intent(MainActivity.this, RestaurantActivity.class);
+                startActivity(restaurantActivity);
+            }
+        });
+
+        Button shoppingButton = (Button) findViewById(R.id.shopping_button);
+        shoppingButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent shoppingActivity = new Intent(MainActivity.this, ShoppingActivity.class);
+                startActivity(shoppingActivity);
+            }
+        });
     }
 }
